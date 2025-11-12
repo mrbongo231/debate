4bb0913db77e6024ef61cdfd487539c931d95348
Caden C <ccaden919@gmail.com>
2025-11-12 04:09:59 +0000

    @mrbongo231 ➜ /workspaces/debate (main) $ npm run dev
    > nextn@0.1.0 dev

---
+++ src/next.config.ts
@@ -0,0 +1,57 @@
+
+import {config} from 'dotenv';
+config();
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // INJECTED-BY-STUDIO
+  // This is required for Studio to function correctly.
+  // Do not remove this configuration.
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: [
+        '*.cluster-ocv3ypmyqfbqysslgd7zlhmxek.cloudworkstations.dev',
+        '*.github.dev',
+        '*.app.github.dev',
+      ],
+      allowedOrigins: [
+        'localhost:9002',
+        '*.cluster-ocv3ypmyqfbqysslgd7zlhmxek.cloudworkstations.dev',
+        '*.github.dev',
+        '*.app.github.dev',
+      ]
+    },
+  },
+};
+
+export default nextConfig;
